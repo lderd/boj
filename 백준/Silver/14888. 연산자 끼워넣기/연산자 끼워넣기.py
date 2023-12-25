2521c3753942c21870b8ee2b6efd021addbc9598
lderd <119023528+lderd@users.noreply.github.com>
2023-12-25 17:42:28 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 212 ms, Memory: 112736 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,96 +1,29 @@
-import sys
-input = sys.stdin.readline
+def solve(idx, val, plus, minus, mul, div):
+    global max_answer, min_answer
+    if idx >= n:
+        if val > max_answer:
+            max_answer = val
+        if val < min_answer:
+            min_answer = val
+        return
+    if plus < oper[0]:
+        solve(idx+1, val+a[idx], plus+1, minus, mul, div)
+    if minus < oper[1]:
+        solve(idx+1, val-a[idx], plus, minus+1, mul, div)
+    if mul < oper[2]:
+        solve(idx+1, val*a[idx], plus, minus, mul+1, div)
+    if div < oper[3]:
+        if val < 0:
+            solve(idx+1, -(-val // a[idx]), plus, minus, mul, div+1)
+        else:
+            solve(idx+1, val // a[idx], plus, minus, mul, div+1)
+
 
 n = int(input())
-num = list(map(int, input().split()))
-arr = list(map(int, input().split()))
-
-
-operand = []
-for i in range(4):
-    if i == 0:
-        for j in range(arr[i]):
-            operand.append('+')
-
-
-    elif i == 1:
-        for j in range(arr[i]):
-            operand.append('-')
-
-    elif i == 2:
-        for j in range(arr[i]):
-            operand.append('*')
-
-
-    else:
-        for j in range(arr[i]):
-            operand.append('/')
-
-
-# print(operand)
-
-
-visited = [0] * (n - 1)
-minn = 10 ** 10
-maxx = -(10 ** 10)
-
-def dfs(L, check):
-    global maxx
-    global minn
-
-    if L == n - 1:
-        # print('check', check)
-        if check >= maxx:
-            maxx = check
-
-        if check <= minn:
-            minn = check
-        check = num[0]
-        # print('maxx, minn', maxx, minn)
-        # print('======끝=======')
-        return (maxx, minn)
-
-        minn = 10 ** 10
-        maxx = - (10 ** 10)
-
-    else:
-        #operand갯수만큼
-        for i in range(n - 1):
-            # print(i)
-            if visited[i] == 0:
-                if operand[i] == '+':
-                    # print(check, end=' ')
-                    Ncheck = check + num[L + 1]
-                    # print('+', num[L + 1], '=', check)
-
-                elif operand[i] == '-':
-                    # print(check, end=' ')
-                    Ncheck = check - num[L + 1]
-                    # print('-', num[L + 1], '=', check)
-
-                elif operand[i] == '*':
-                    # print(check, end=' ')
-                    Ncheck = check * num[L + 1]
-                    # print('*', num[L + 1], '=', check)
-
-                else:
-                    if check >= 0:
-                        # print(check, end=' ')
-                        Ncheck = check // num[L + 1]
-                        # print('//', num[L + 1], '=', check)
-
-                    else:
-                        Ncheck = -((check * -1) // num[L + 1])
-
-
-                # print(result)
-                visited[i] = 1
-                # print('[check b4 dfs]', check)
-                dfs(L + 1, Ncheck)
-                visited[i] = 0
-                Ncheck = check
-
-
-dfs(0, num[0])
-print(maxx)
-print(minn)
+a = list(map(int, input().split()))
+oper = list(map(int, input().split()))
+max_answer = -1000000000
+min_answer = 1000000000
+solve(1, a[0], 0, 0, 0, 0)
+print(max_answer)
+print(min_answer)
