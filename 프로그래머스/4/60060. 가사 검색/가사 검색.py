962e545d50127446bd656c0356d47d64d702db3a
lderd <119023528+lderd@users.noreply.github.com>
2024-01-08 21:38:50 +0900

    [level 4] Title: 가사 검색, Time: 7092.18 ms, Memory: 1.28 GB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.py
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.py
@@ -1,77 +1,52 @@
+class Node(object):
+    def __init__(self):
+        self.data = {}
+        self.children = {}
+
+
+class Trie:
+    def __init__(self):
+        self.head = Node()
+
+    def insert(self, word):
+        current_node = self.head
+        l = len(word)
+        for char in word:
+            if l not in current_node.data:
+                current_node.data[l] = 0
+            if char not in current_node.children:
+                current_node.children[char] = Node()
+            current_node.data[l] += 1
+            current_node = current_node.children[char]
+            l -= 1
+
+    def search(self, word):
+        current_node = self.head
+        l = len(word)
+        for char in word:
+            if char == '?':
+                break
+            else:
+                if char in current_node.children:
+                    current_node = current_node.children[char]
+                    l -= 1
+                else:
+                    return 0
+        if l in current_node.data:
+            return current_node.data[l]
+        return 0
+
+
 def solution(words, queries):
     answer = []
-    l = len(words)
-    right_mark = sorted(words, key=lambda x:(len(x), x))
-    left_mark = sorted(map(lambda x:''.join(reversed(x)), words), key=lambda x:(len(x), x))
+    pre = Trie()
+    suffix = Trie()
+    for word in words:
+        pre.insert(word)
+        suffix.insert(''.join(reversed(word)))
     for q in queries:
         if q[0] == '?':
-            q_len = len(q)
-            left_word = ''.join(reversed(q)).replace('?', 'a')
-            right_word = ''.join(reversed(q)).replace('?', 'z')
-            s = 0
-            e = l - 1
-            while s <= e:
-                mid = (s + e) // 2
-                now_word = left_mark[mid]
-                if len(now_word) > q_len:
-                    e = mid - 1
-                elif len(now_word) < q_len:
-                    s = mid + 1
-                else:
-                    if now_word >= left_word:
-                        e = mid - 1
-                    else:
-                        s = mid + 1
-            left_idx = e
-            s = 0
-            e = l - 1
-            while s <= e:
-                mid = (s + e) // 2
-                now_word = left_mark[mid]
-                if len(now_word) > q_len:
-                    e = mid - 1
-                elif len(now_word) < q_len:
-                    s = mid + 1
-                else:
-                    if now_word >= right_word:
-                        e = mid - 1
-                    else:
-                        s = mid + 1
-            right_idx = e
-            answer.append(right_idx - left_idx)
+            answer.append(suffix.search(''.join(reversed(q))))
         else:
-            q_len = len(q)
-            left_word = q.replace('?', 'a')
-            right_word = q.replace('?', 'z')
-            s = 0
-            e = l - 1
-            while s <= e:
-                mid = (s + e) // 2
-                now_word = right_mark[mid]
-                if len(now_word) > q_len:
-                    e = mid - 1
-                elif len(now_word) < q_len:
-                    s = mid + 1
-                else:
-                    if now_word >= left_word:
-                        e = mid - 1
-                    else:
-                        s = mid + 1
-            left_idx = e
-            s = 0
-            e = l - 1
-            while s <= e:
-                mid = (s + e) // 2
-                now_word = right_mark[mid]
-                if len(now_word) > q_len:
-                    e = mid - 1
-                elif len(now_word) < q_len:
-                    s = mid + 1
-                else:
-                    if now_word >= right_word:
-                        e = mid - 1
-                    else:
-                        s = mid + 1
-            right_idx = e
-            answer.append(right_idx - left_idx)
+            answer.append(pre.search(q))
     return answer
