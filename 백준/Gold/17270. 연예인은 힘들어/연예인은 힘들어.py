c543a392f77e267149c616964e91c1430311173b
lderd <119023528+lderd@users.noreply.github.com>
2024-02-28 14:26:43 +0900

    [Gold III] Title: 연예인은 힘들어, Time: 132 ms, Memory: 110808 KB -BaekjoonHub

--- 백준/Gold/17270. 연예인은 힘들어/연예인은 힘들어.py
+++ 백준/Gold/17270. 연예인은 힘들어/연예인은 힘들어.py
@@ -1,55 +1,64 @@
 import sys
-from heapq import heappop, heappush
+
 input = sys.stdin.readline
+
+import heapq
+
 v, m = map(int, input().split())
-arr = [[] for _ in range(v+1)]
+
+inf = int(1e10)
+
+arr = [[] for i in range(v + 1)]
+
+distance_j = [inf] * (v + 1)
+distance_s = [inf] * (v + 1)
+
 for _ in range(m):
     a, b, c = map(int, input().split())
     arr[a].append((b, c))
     arr[b].append((a, c))
+
 j, s = map(int, input().split())
 
-q = [(0, j)]
-j_dist = [10000000] * (v+1)
-j_dist[j] = 0
-j_checked = set(range(1, v+1))
-while q and j_checked:
-    c_dist, c = heappop(q)
-    if c not in j_checked: continue
-    j_checked.remove(c)
-    for n, d in arr[c]:
-        if c_dist + d < j_dist[n]:
-            heappush(q, (c_dist + d, n))
-            j_dist[n] = c_dist + d
 
-q = [(0, s)]
-s_dist = [10000000] * (v+1)
-s_dist[s] = 0
-s_checked = set(range(1, v+1))
-while q and s_checked:
-    c_dist, c = heappop(q)
-    if c not in s_checked: continue
-    s_checked.remove(c)
-    for n, d in arr[c]:
-        if c_dist + d < s_dist[n]:
-            heappush(q, (c_dist + d, n))
-            s_dist[n] = c_dist + d
+def dijkstra(start, distance):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
 
-answer = -1
-j_d = 10000000
-s_d = 10000000
-dist = 10000000
-for i in range(1, v+1):
-    if i == j or i == s: continue
-    if j_dist[i] + s_dist[i] < dist:
-        dist = j_dist[i] + s_dist[i]
-for i in range(1, v+1):
-    if i == j or i == s: continue
-    a, b = j_dist[i], s_dist[i]
-    if a > b: continue
-    if a + b == dist:
-        if a < j_d:
-            answer = i
-            j_d = a
-            s_d = b
-print(answer)
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in arr[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(j, distance_j)
+dijkstra(s, distance_s)
+
+min = inf
+for i in range(1, v + 1):
+    if i != j and i != s:
+        if distance_j[i] + distance_s[i] < min:
+            min = distance_j[i] + distance_s[i]
+
+x = inf
+# y = inf
+result = -1
+
+for i in range(v, 0, -1):
+    if i != j and i != s:
+        if distance_j[i] <= distance_s[i] and distance_j[i] + distance_s[i] == min:
+            if distance_j[i] <= x:
+                x = distance_j[i]
+                # y = distance_s[i]
+                result = i
+
+print(result)
