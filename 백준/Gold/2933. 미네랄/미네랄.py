91d9fe7679bd585a0e7785a06d9e374c7bbe511a
lderd <119023528+lderd@users.noreply.github.com>
2024-07-11 15:09:26 +0900

    [Gold I] Title: 미네랄, Time: 312 ms, Memory: 117332 KB -BaekjoonHub

--- 백준/Gold/2933. 미네랄/미네랄.py
+++ 백준/Gold/2933. 미네랄/미네랄.py
@@ -1,67 +1,59 @@
-from collections import deque
-def down(a, b):
-    cluster = [(a, b)]
-    bottom_cluster = [-1] * c
-    bottom_cluster[b] = a
-    q = deque([(a, b)])
-    while q:
-        ci, cj = q.popleft()
-        for di, dj in d:
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < r and 0 <= nj < c and cave[ni][nj] == 'x' and not checked[ni][nj]:
-                cluster.append((ni, nj))
-                q.append((ni, nj))
-                checked[ni][nj] = True
-                if bottom_cluster[nj] < ni:
-                    bottom_cluster[nj] = ni
-    down_cnt = 100
-    for j in range(c):
-        if bottom_cluster[j] > -1:
-            for i in range(bottom_cluster[j] + 1, r):
-                if cave[i][j] == 'x':
-                    tmp = i - bottom_cluster[j] - 1
-                    break
-            else:
-                tmp = r - bottom_cluster[j] - 1
-            if tmp < down_cnt:
-                down_cnt = tmp
-    if down_cnt > 0:
-        cluster.sort(reverse=True)
-        for i, j in cluster:
-            cave[i][j] = '.'
-            cave[i+down_cnt][j] = 'x'
-        return True
-    return False
-
-
 r, c = map(int, input().split())
 cave = [list(input()) for _ in range(r)]
 n = int(input())
-stick = list(map(int, input().split()))
-d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+hs = list(map(int, input().split()))
 for idx in range(n):
-    for i in range(c):
-        # 홀수(오른쪽에서 왼쪽으로 날아감)
-        if idx % 2:
-            if cave[r-stick[idx]][-i-1] == 'x':
-                cave[r-stick[idx]][-i-1] = '.'
+    h = hs[idx]
+    bi = bj = -1
+    # 홀수면 오른쪽에서 발사
+    if idx % 2:
+        for j in range(c-1, -1, -1):
+            if cave[r-h][j] == 'x':
+                bi, bj = r-h, j
                 break
-        # 짝수(왼쪽에서 오른쪽으로 날아감)
-        else:
-            if cave[r-stick[idx]][i] == 'x':
-                cave[r-stick[idx]][i] = '.'
-                break
-    else: continue
-    checked = [[False] * c for _ in range(r)]
-    flag = False
-    for i in range(r):
+    # 짝수면 왼쪽에서 발사
+    else:
         for j in range(c):
-            if not checked[i][j] and cave[i][j] == 'x':
-                checked[i][j] = True
-                if down(i, j):
-                    flag = True
-                    break
-        if flag:
-            break
+            if cave[r-h][j] == 'x':
+                bi, bj = r-h, j
+                break
+    if bi == bj == -1: continue
+    cave[bi][bj] = '.'
+    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    checked = set()
+    for bdi, bdj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        ci, cj = bi+bdi, bj+bdj
+        # 깨진 미네랄 주변에 미네랄이 있다면 해당 클러스터는 공중에 뜨게 될 가능성이 있다
+        if 0 <= ci < r and 0 <= cj < c and (ci, cj) not in checked and cave[ci][cj] == 'x':
+            checked.add((ci, cj))
+            cluster = {(ci, cj)}
+            down = True
+            q = [(ci, cj)]
+            while q:
+                i, j = q.pop()
+                for di, dj in d:
+                    ni, nj = i+di, j+dj
+                    if 0 <= ni < r and 0 <= nj < c and cave[ni][nj] == 'x' and (ni, nj) not in cluster:
+                        q.append((ni, nj))
+                        if ni == r - 1:
+                            down = False
+                        cluster.add((ni, nj))
+                        if (ni == bi - 1 and nj == bj) or (ni == bi + 1 and nj == bj) or (ni == bi and nj == bj - 1) or (ni == bi and nj == bj + 1):
+                            checked.add((ni, nj))
+            flag = False
+            while down:
+                cluster_list = sorted(cluster, reverse=True)
+                new_cluster = set()
+                for i, j in cluster_list:
+                    new_cluster.add((i+1, j))
+                    cave[i+1][j] = 'x'
+                    cave[i][j] = '.'
+                    if i + 1 == r - 1 or ((i + 1, j) not in cluster and cave[i+2][j] == 'x'):
+                        down = False
+                        flag = True
+                cluster = new_cluster
+            # 클러스터는 한번에 하나만 떨어진다
+            if flag:
+                break
 for i in range(r):
     print(''.join(cave[i]))
