ae90f6c8f107a73e0783c4dcd7b492793e3be3d2
lderd <119023528+lderd@users.noreply.github.com>
2024-08-27 20:12:05 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 1692 ms, Memory: 191960 KB -BaekjoonHub

---
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def init(start, end, index):
+    if start == end:
+        tree[index] = [arr[start], arr[start]]
+        return tree[index]
+    mid = (start + end) // 2
+    a, b = init(start, mid, index * 2)
+    c, d = init(mid+1, end, index * 2 + 1)
+    tree[index] = [min(a, c), max(b, d)]
+    return tree[index]
+
+
+def solve(start, end, index, left, right):
+    if left > end or right < start:
+        return [1000000001, 0]
+    if left <= start and right >= end:
+        return tree[index]
+    mid = (start + end) // 2
+    a, b = solve(start, mid, index * 2, left, right)
+    c, d = solve(mid + 1, end, index * 2 + 1, left, right)
+    return [min(a, c), max(b, d)]
+
+
+n, m = map(int, input().split())
+tree = [[0, 0] for _ in range(n * 4)]
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+init(0, n-1, 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(*solve(0, n-1, 1, a-1, b-1))
