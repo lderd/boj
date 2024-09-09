4e7feaa780c3255ac29748eaaf158d9e07692dc0
lderd <119023528+lderd@users.noreply.github.com>
2024-09-09 20:02:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 356 ms, Memory: 251768 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 def union(a, b):
     a = find(a)
     b = find(b)
@@ -11,24 +12,28 @@ def union(a, b):
 
 def find(a):
     if p[a] == a:
-        return p[a]
+        return a
     p[a] = find(p[a])
     return p[a]
 
 
 v, e = map(int, input().split())
 answer = 0
-p = list(range(v+1))
+p = list(range(v))
 q = []
 for _ in range(e):
     a, b, c = map(int, input().split())
     q.append((c, a-1, b-1))
 q.sort()
+rest = v - 1
 for j in range(e):
     c, a, b = q[j]
     a = find(a)
     b = find(b)
     if a != b:
+        rest -= 1
         answer += c
         union(a, b)
+    if rest == 0:
+        break
 print(answer)
